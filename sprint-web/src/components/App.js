906f9403fe2f8f67cc2b82fe6d2ef39e29e6b79c
Vivek Reddy Doma <VivekReddy.Doma@sprint.com>
2018-08-17 16:12:55 -0500

    added devices code

--- sprint-web/src/components/App.js
+++ sprint-web/src/components/App.js
@@ -2,84 +2,31 @@ import React, { Component } from 'react';
 
 // Import widgets being used in this component
 import NumberWidgetContainer from '../components/NumberWidgetContainer';
+import DevicesWidgetContainer from '../components/DevicesWidgetContainer';
 import ListWidgetContainer from '../components/ListWidgetContainer';
 import GraphWidgetContainer from '../components/GraphWidgetContainer';
-
-
-import axios from "axios";
-import ContactList from "../components/ContactList";
-import Widget from '../components/Widget';
-//2  import NumberDisplay from '../components/NumberDisplay';
-//3  import NumberWidget from '../components/NumberWidget';
 
 // Add in styles
 import '../styles/App.css';
 
 class App extends Component {
-
-
-
-    // default state object
-    state = {
-        contacts: []
-    };
-
-    componentDidMount() {
-        axios
-            .get("https://my-json-server.typicode.com/vivek-2/devices/Devices")
-            .then(response => {
-                // create an array of contacts only with relevant data
-                const newContacts = response.data.map(c => {
-                    return {
-                        id: c.id,
-                        name: c.title,
-                        colors: c.color,
-                        memo: c.memory,
-                        price: c.srp
-                    };
-                });
-
-                // create a new "state" object without mutating
-                // the original state object.
-                const newState = Object.assign({}, this.state, {
-                    contacts: newContacts
-                });
-
-                // store the new state object in the component's state
-                this.setState(newState);
-            })
-            .catch(error => console.log(error));
-    }
-
-
-
-
-
-
     render() {
         return (
             <div className="App">
                 {/* Add Widgets to display */}
                 <ListWidgetContainer href="http://localhost:3001/stats/top" heading="Top Ticket Answerers" rowspan={3} />
+                <NumberWidgetContainer href="http://localhost:8085/bob/bob/1.0/api/customer/01" heading="Open Ticket Total" />
                 <GraphWidgetContainer href="http://localhost:3001/tickets/progression" heading="Tickets Over Time" colspan={2} rowspan={2} />
-                <NumberWidgetContainer href="http://localhost:3001/tickets/today" heading="Tickets Opened Today" colspan={1} rowspan={1} />
+                <NumberWidgetContainer href="http://localhost:3001/tickets/today" heading="Tickets Opened Today" />
                 <NumberWidgetContainer href="http://localhost:3001/tickets/urgent" heading="Tickets Marked 'Urgent'" />
+                <DevicesWidgetContainer href="http://localhost:3001/tickets/devices" heading="Devices 'Urgent work'" />
                 <NumberWidgetContainer href="http://localhost:3001/stats/response" heading="4 Hour Response %" />
                 <NumberWidgetContainer href="http://localhost:3001/stats/solved" heading="7 Day Solved %" />
-
-
-                <Widget colspan={2} >
-                    <h1 id="widgetHeading"> Equipments </h1>
-                    <ContactList contacts={this.state.contacts} />
-                </Widget>
-
-
-
+                <NumberWidgetContainer href="http://localhost:8085/bob/bob/1.0/api/customer/01" heading="From My Rest API" />
             </div>
-
-
         );
     }
 }
 
 export default App;
+
---
+++ sprint-web/src/components/DevicesWidgetContainer.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react';
+
+// Import request module
+import axios from 'axios';
+
+// Import components
+import NumberWidget from '../components/NumberWidget';
+
+class DevicesWidgetContainer extends Component {
+    constructor() {
+        super();
+
+        // Set initial state
+        this.state = {
+            loading: false,
+            min: undefined,
+            max: undefined,
+            value: undefined
+        }
+
+        // Bind function to refer to component
+        this.getData = this.getData.bind(this);
+    }
+
+    // Fetch data when the component is added
+    componentDidMount() {
+        this.getData().then(_ => {
+            // Re-fetch every minute
+            this.interval = setInterval(this.getData, 60000);
+        });
+    }
+
+    // Fetch new data
+    getData() {
+        // Tell the Widget component we're currently loading
+        this.setState({ loading: true });
+
+        // Fetch data
+        return axios.get(this.props.href)
+            .then(response => {
+                // Build a new state
+                let newState = { loading: false };
+
+                // Populate state with new data
+                if (response.data.hasOwnProperty("min")) {
+                    newState["min"] = response.data.min;
+                }
+                if (response.data.hasOwnProperty("max")) {
+                    newState["max"] = response.data.max;
+                }
+
+                if (response.data.hasOwnProperty("value")) {
+                    newState["value"] = response.data.value;
+                }
+                if (response.data.hasOwnProperty("Price")) {
+                    newState["max"] = response.data.Price;
+                }
+                if (response.data.hasOwnProperty("SKU")) {
+                    newState["value"] = response.data.SKU;
+                }
+
+                // Update state with data
+                this.setState(newState);
+            })
+            .catch(error => {
+                // At least tell the Widget component we have stopped loading
+                console.log(error);
+                this.setState({ loading: false });
+            });
+    }
+
+    render() {
+        return (
+            // Render the number widget
+            <NumberWidget heading={this.props.heading} colspan={this.props.colspan} rowspan={this.props.rowspan} min={this.state.min} max={this.state.max} value={this.state.value} loading={this.state.loading} />
+        );
+    }
+}
+
+// Enforce the type of props to send to this component
+DevicesWidgetContainer.propTypes = {
+    heading: React.PropTypes.string,
+    colspan: React.PropTypes.number,
+    rowspan: React.PropTypes.number,
+    href: React.PropTypes.string.isRequired
+}
+
+export default DevicesWidgetContainer;
+
