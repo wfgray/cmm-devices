d479690f7d20c3c334c48a487a5065fbaf81be68
Saisurya Pamidipati <Saisurya.Pamidipati@sprint.com>
2018-08-17 09:48:57 -0500

    dev_db_update

--- sprint-rest-api-device/src/main/java/sprint/rest/api/device/Application.java
+++ sprint-rest-api-device/src/main/java/sprint/rest/api/device/Application.java
@@ -1,4 +1,4 @@
-package sprint.rest.api.device.app;
+package sprint.rest.api.device;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
